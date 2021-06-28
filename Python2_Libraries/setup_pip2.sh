b0ef2aa12b328e4024f8cf720fede26a1285b6f6
Aziz Kaplan <AzizKpln@protonmail.com>
2021-06-28 19:57:54 +0300

    Add files via upload

---
+++ Python2_Libraries/setup_pip2.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install python python-dev python-setuptools libpcap0.8-dev libnetfilter-queue-dev libssl-dev libjpeg-dev libxml2-dev libxslt1-dev  libcapstone-dev libffi-dev file 
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+python get-pip.py
+
