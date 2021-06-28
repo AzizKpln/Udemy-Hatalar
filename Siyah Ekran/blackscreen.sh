a453246c52babb5710ffff4a760db45ea57d9729
Aziz Kaplan <AzizKpln@protonmail.com>
2021-06-28 19:40:33 +0300

    Add files via upload

---
+++ Siyah Ekran/blackscreen.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install virtualbox-dkms
+sudo apt install virtualbox-guest-dkms
+sudo apt install virtualbox-guest-additions-iso
+sudo apt install open-vm-tools-desktop fuse
